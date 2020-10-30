793fcb96013c209c852b002127d6abe23d644100
Somsubhra Das <das.somsubhra1@gmail.com>
2020-10-30 14:43:30 +0530

    project complete

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -22,27 +22,27 @@ export class ItemsController {
   //     console.log(req);
   //     return res.send('Hello world');
   //   }
-  async findAll(): Promise<Item[]> {
+  findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id): Promise<Item> {
+  findOne(@Param('id') id): Promise<Item> {
     return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): String {
-    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
   }
 
   @Delete(':id')
-  delete(@Param('id') id): string {
-    return `Delete ${id}`;
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
   }
 
   @Put(':id')
-  update(@Param('id') id, @Body() updateItemDto: CreateItemDto): string {
-    return `Update ${id} - Name: ${updateItemDto.name}`;
+  update(@Param('id') id, @Body() updateItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
   }
 }
